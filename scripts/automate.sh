8524bfff599d9da81a3e059d12a2e8a064f2bf0b
Grzegorz Nowak <strange3studio@gmail.com>
2022-02-12 05:39:49 +0100

    wip CI

--- scripts/automate.sh
+++ scripts/automate.sh
@@ -88,7 +88,11 @@ fi
 
 # delete root folder to get clean slate only if we didn't ask it not to do that
 # invoke as `./automate 1` to effectively stop flushing the underlying private blockchain
-[[ $2 != "1" ]] && rm -rf $ROOT
+if [ $2 != "1" ]; then
+  echo "We opted to not remove the existing state of the blockchain, skipping the removal"
+else
+  rm -rf $ROOT
+fi
 
 # run script to create config
 "${SCRIPT_PATH}"/mkfiles.sh alonzo
