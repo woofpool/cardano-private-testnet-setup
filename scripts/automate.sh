3f8872517c4195255015c0032ab6c5c959ebb8d0
Kurt Hartmann <extramileit@gmail.com>
2022-08-22 16:17:15 -0500

    Work-in-progress commit to enable running a private testnet in the babbage era - update mkfiles.sh with script from IOHK cardano-node project for babbage and modify to be compatible with running cardano-db-sync - add the alonzo-babbage-test-genesis.json configuration file copied from IOHK cardano-node project - update instructions

--- scripts/automate.sh
+++ scripts/automate.sh
@@ -3,11 +3,10 @@
 set -e
 
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-. "${SCRIPT_PATH}"/config-read.shlib; # load the config library functions
 
-ROOT="$(config_get ROOT)";
-
-export CARDANO_NODE_SOCKET_PATH=$ROOT/node-bft1/node.sock
+ROOT=private-testnet
+export CARDANO_NODE_SOCKET_PATH=$ROOT/node-spo1/node.sock
+echo "path: $CARDANO_NODE_SOCKET_PATH"
 
 wait_until_socket_detected()
 {
@@ -51,69 +50,28 @@ restart_nodes_in_bg()
   echo "Nodes restarted"
 }
 
-run_update_script()
-{
-  echo "request to run update-$1 script"
-  if [ -z "$2" ]
-  then
-    "${SCRIPT_PATH}"/update-$1.sh
-  else
-    "${SCRIPT_PATH}"/update-$1.sh $2
-  fi
-  echo "Sleep 5 secs to ensure update is received"; sleep 5
-  echo "update-$1 script completed"
-}
-
-wait_for_epoch_to_advance()
-{
-  target_epoch_no=$(expr $epoch_no + $1)
-  echo "Waiting until epoch $target_epoch_no"
-  while [ $epoch_no -lt $target_epoch_no ]; do
-    echo "Sleep 30 secs"; sleep 30
-    epoch_no=$(cardano-cli query tip --testnet-magic 42 | jq '.epoch')
-  done
-  echo "reached epoch: $epoch_no"
-}
-
 query_tip()
 {
   cardano-cli query tip --testnet-magic 42
 }
 
-running_nodes_cnt=$( ps -ef | grep 'cardano-node' | grep -v grep | wc -l )
-if [ $running_nodes_cnt -gt 0 ]; then
-  echo "Script aborted, since running cardano nodes have been found."
-  exit
-fi
-
 # rerunning the script should always result in restarting nodes
 # this way we can at least control this a bit more, compared to if users where to do it manually
 kill_running_nodes
 
-# delete root folder to get clean slate only if we didn't ask it not to do that
-# invoke as `./automate 1` to effectively stop flushing the underlying private blockchain
-if [ "$1" = "1" ]; then
-  echo "We opted to not remove the existing state of the blockchain, skipping the removal"
-else
-  rm -rf $ROOT
-fi
+rm -rf $ROOT
 
 # run script to create config
-"${SCRIPT_PATH}"/mkfiles.sh alonzo
+"${SCRIPT_PATH}"/mkfiles.sh
 
 restart_nodes_in_bg
 echo
 wait_until_socket_detected
 
-if [ -f $ROOT/ready.flag ]; then
-  echo "ready.flag already set, no need to consume the genesis utxo"
-else
-  echo "we're booting from a pristine state; consuming the genesis utxo"
-  run_update_script "1"
-fi
+#query_tip
+cli_version=$(cardano-cli version)
+echo "CLI Version = $cli_version"
 
-
-query_tip
 echo
 current_era=$( cardano-cli query tip --testnet-magic 42 | jq '.era' )
 protocol_version=$( cardano-cli query protocol-parameters --testnet-magic 42 | jq '.protocolVersion.major' )
@@ -121,6 +79,4 @@ echo "Nodes are running in era: $current_era, major protocol version: $protocol_
 echo
 echo "Congrats! Your network is ready for use!"
 
-# the simplest possible state-keeping registry. An alternative would be sqlite, but even that is probably an overkill
-touch $ROOT/ready.flag
 wait
